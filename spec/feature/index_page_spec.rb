af1eb11c716a9212c3c7cb958965814502635a2e
Jamie Bowers <jamiebowers3@gmail.com>
2021-11-02 15:30:29 +0000

    Task 7 - Implemented database to app

--- spec/feature/index_page_spec.rb
+++ spec/feature/index_page_spec.rb
@@ -2,9 +2,7 @@ feature 'index page' do
   
   scenario 'User can see bookmarks stored' do 
     visit('/bookmarks')
-    expect(page).to have_content "http://www.makersacademy.com"
-    expect(page).to have_content "http://www.destroyallsoftware.com"
-    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://makersacademy.com http://www.google.com http://www.destroyallsoftware.com"
   end 
 
 end 
